be15adabd1babca6b17e38279bceff424394827f
saurav-inde <sauravmiit@gmail.com>
2023-11-25 19:28:16 +0530

    port settings changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const url = '0.0.0.0'
-const port = 3000;
+const port = process.env.PORT || 3030;
 
 app.get('/', (req, res)=>{
     res.send('Hello World');
